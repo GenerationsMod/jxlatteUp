c3160dbee65252196a056b84aecb4b0b0034aadb
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-03-27 11:42:26 -0400

    Add Gradle Build Script

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+    idea
+    `maven-publish`
+}
+
+group = "com.thebombzen"
+version = "2.0.0"
+
+repositories.mavenCentral()
+dependencies.compileOnly("org.jetbrains:annotations:24.1.0")
+
+publishing {
+    publications.create<MavenPublication>("mavenCommon").from(components["java"])
+
+    repositories {
+        mavenLocal()
+        maven {
+            val releasesRepoUrl = "https://maven.generations.gg/releases"
+            val snapshotsRepoUrl = "https://maven.generations.gg/snapshots"
+            url = uri(if (project.version.toString().endsWith("SNAPSHOT") || project.version.toString().startsWith("0")) snapshotsRepoUrl else releasesRepoUrl)
+            name = "Generations-Repo"
+            credentials {
+                username = getGensCredentials().first
+                password = getGensCredentials().second
+            }
+        }
+    }
+}
+
+private fun getGensCredentials(): Pair<String?, String?> {
+    val username = (project.findProperty("gensUsername") ?: System.getenv("GENS_USERNAME") ?: "") as String?
+    val password = (project.findProperty("gensPassword") ?: System.getenv("GENS_PASSWORD") ?: "") as String?
+    return Pair(username, password)
+}
